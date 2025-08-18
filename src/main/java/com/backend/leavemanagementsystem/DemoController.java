0cbd8be0a3ef127ee7618043abbc227f57fc57d2
shivanshow <singhshivanshu337@gmail.com>
2025-08-18 19:35:26 +0530

    fix: apply spotless formatting

--- src/main/java/com/backend/leavemanagementsystem/DemoController.java
+++ src/main/java/com/backend/leavemanagementsystem/DemoController.java
@@ -1,11 +1,10 @@
 package com.backend.leavemanagementsystem;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController()
 @RequestMapping("/check")
